2c2da21da2211e015470ff729e8a3fa2026607b3
Sara Marshall <saramarshallux@gmail.com>
2021-09-30 17:59:55 -0500

    change to local storage

--- script.js
+++ script.js
@@ -1,87 +1,81 @@
-(async function (){
-  
+
+const taskInput = document.getElementById("new");
+const addButton = document.getElementById("add");
+const tasks = document.getElementById("tasks");
 
 
-  const taskInput= document.getElementById("new");
-  const addButton= document.getElementById("add");
-  const tasks= document.getElementById("tasks");
+addButton.addEventListener("click", addNewItem);
+taskInput.addEventListener("keyup", processKeyPress);
 
+const items = getItems();
 
-  addButton.addEventListener("click", addNewItem);
-  taskInput.addEventListener ("keyup", processKeyPress);
+for (let item of items) {
+  const li = createElementForTask(item);
+  tasks.appendChild(li);
+}
 
-  const items= await getItems();
+function processKeyPress(event) {
+  addButton.disabled = event.target.value.trim() === "";
 
-  for(let item of items) {
-    const li= createElementForTask(item);
-    tasks.appendChild(li);
+  if (event.key === "Enter" && !addButton.disabled) {
+    addNewItem();
   }
+}
 
-  function processKeyPress(event){
-    addButton.disabled = event.target.value.trim()==="";
+function createElementForTask(item) {
+  const template = document.getElementById("taskTemplate");
+  const newListItem = template.content.cloneNode(true);
 
-    if (event.key==="Enter" && !addButton.disabled) {
-      addNewItem();
-    }
-  }
+  const checkbox = newListItem.querySelector(".item-check");
+  const text = newListItem.querySelector(".item-text");
+  const deleteButton = newListItem.querySelector(".delete");
 
-  function createElementForTask(item) {
-    const template= document.getElementById("taskTemplate");
-    const newListItem= template.content.cloneNode(true);
+  text.innerText = item.value;
+  checkbox.checked = item.complete;
 
-    const checkbox= newListItem.querySelector(".item-check");
-    const text= newListItem.querySelector(".item-text");
-    const deleteButton = newListItem.querySelector (".delete");
-
-    text.innerText = item.value;
-    checkbox.checked = item.complete;
-
-    deleteButton.onclick = function (event){
-      event.target.closest ('li').remove();
-      items.splice(items.indexOf(item), 1);
-      saveItems();
-    }
-
-    checkbox.onchange = function (event){
-      item.complete = event.target.checked;
-      saveItems();
-    }
-
-    return newListItem;
-
-  }
-
-  function addNewItem() {
-    const task = {
-      value: taskInput.value,
-      complete: false
-    };
-
-    items.push(task);
+  deleteButton.onclick = function (event) {
+    event.target.closest('li').remove();
+    items.splice(items.indexOf(item), 1);
     saveItems();
-
-    let newItem = createElementForTask(task);
-    tasks.appendChild(newItem);
-
-    taskInput.value = "";
-    taskInput.focus();
   }
 
-  async function getItems() {
-    const request = await fetch("https://todo-api-ff.azurewebsites.net/api/todo", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/JSON"                         
-      }                       
-       });
-        const itemsJSON = (await request.text() || "[]");
-        return JSON.parse(itemsJSON);
+  checkbox.onchange = function (event) {
+    item.complete = event.target.checked;
+    saveItems();
   }
 
-  async function saveItems() {
-    const data = JSON.stringify (items);
-    localStorage.setItem("items", data);
+  return newListItem;
 
-  }
+}
 
-}());
+function addNewItem() {
+  const task = {
+    value: taskInput.value,
+    complete: false,
+  };
+  
+  items.push(task);
+  saveItems();
+
+  let newItem = createElementForTask(task);
+  tasks.appendChild(newItem);
+
+  taskInput.value = "";
+  taskInput.focus();
+}
+
+function getItems() {
+  const noItemsFound = "[]";
+  const itemsJSON = localStorage.getItem("items") || noItemsFound;
+  return JSON.parse (itemsJSON);
+}
+
+function saveItems() {
+  const data = JSON.stringify (items);
+  localStorage.setItem("items", data);
+
+}
+
+localStorage.setItem("message");
+
+const item = localStorage.getItem("message")
